821ad712c9bd700cd5038590236e29e86e23d93f
Ruslan Dolibskyi <dolibskiyruslan.gmail.com>
2024-07-28 19:15:29 +0300

    HW 1

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,29 @@
+import styles from './TransactionHistory.module.css';
+
+const capitalizeFirstLetter = string => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+const TransactionHistory = ({ items }) => {
+  return (
+    <table className={styles.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr key={id}>
+            <td>{capitalizeFirstLetter(type)}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default TransactionHistory;
